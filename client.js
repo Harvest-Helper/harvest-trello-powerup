aad32eff444ac00b8912e111d4170ce9d17d3564
Michael <michael@Michaels-MacBook-Pro.local>
2025-09-01 13:00:09 -0700

    Add card-detail-badges for different button placement

--- client.js
+++ client.js
@@ -3,9 +3,8 @@ var TrelloPowerUp = window.TrelloPowerUp;
 
 TrelloPowerUp.initialize({
     'card-buttons': function(t, options) {
-        // Return a promise or an array of button objects
-        return Promise.resolve([{
-            icon: './harvest-icon.png',
+        return [{
+            icon: 'https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@6.0.0/svgs/solid/clock.svg',
             text: 'Track Time',
             callback: function(t) {
                 return t.card('name', 'url', 'id')
@@ -23,6 +22,31 @@ TrelloPowerUp.initialize({
                         });
                     });
             }
-        }]);
+        }];
+    },
+    
+    // Try card-detail-badges as well for different placement
+    'card-detail-badges': function(t, options) {
+        return [{
+            title: 'Harvest',
+            text: 'Track Time',
+            icon: 'https://cdn.jsdelivr.net/gh/FortAwesome/Font-Awesome@6.0.0/svgs/solid/clock.svg',
+            callback: function(t) {
+                return t.card('name', 'url', 'id')
+                    .then(function(card) {
+                        var harvestUrl = 'https://platform.harvestapp.com/platform/timer?' + 
+                            'external_item_name=' + encodeURIComponent(card.name) +
+                            '&external_item_id=' + encodeURIComponent(card.id) +
+                            '&external_item_url=' + encodeURIComponent(card.url);
+                        
+                        return t.popup({
+                            title: 'Track Time with Harvest',
+                            url: harvestUrl,
+                            height: 500,
+                            width: 400
+                        });
+                    });
+            }
+        }];
     }
 });
