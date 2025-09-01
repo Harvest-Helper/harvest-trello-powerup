4132e5f6f7c29d240813589f5c9ab06dcd5ca356
Michael <michael@Michaels-MacBook-Pro.local>
2025-09-01 15:10:58 -0700

    Replace test button with proper Track Time button in perfect SubTasks location

--- client.js
+++ client.js
@@ -26,14 +26,28 @@ TrelloPowerUp.initialize({
         }];
     },
     
-    // Test card-detail-badges to see positioning
+    // Track Time button positioned next to SubTasks buttons!
     'card-detail-badges': function(t, options) {
         return [{
-            title: 'Test Badge',
-            text: 'TEST BUTTON',
+            title: 'Time Tracking',
+            text: 'Track Time',
             color: 'orange',
             callback: function(t, opts) {
-                alert('Test badge clicked! This appears at the top of the card back.');
+                return t.card('name', 'url', 'id', 'desc', 'due', 'labels')
+                    .then(function(card) {
+                        var harvestUrl = 'https://platform.harvestapp.com/platform/timer?' + 
+                            'app_name=' + encodeURIComponent('Trello') +
+                            '&permalink=' + encodeURIComponent(card.url) +
+                            '&external_item_name=' + encodeURIComponent(card.name) +
+                            '&external_item_id=' + encodeURIComponent(card.id);
+                        
+                        return t.modal({
+                            url: harvestUrl,
+                            height: 380,
+                            fullscreen: false,
+                            title: 'Track Time with Harvest'
+                        });
+                    });
             }
         }];
     },
